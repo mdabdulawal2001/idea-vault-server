1fcd1d371907236b57a06978cc5769f4c40ff6a9
toha245 <mdabdulawal2001@gmail.com>
2026-08-31 02:18:33 +0600

    uncomment listener

--- index.js
+++ index.js
@@ -951,6 +951,6 @@ async function run() {
 run().catch(console.dir);
 
 // Listener
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
